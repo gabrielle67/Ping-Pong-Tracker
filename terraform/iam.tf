9ae72f5067dbe2c4ac1e466e206be5c710d448ce
gabrielle67 <gdyer8899@gmail.com>
2023-03-30 17:57:41 -0500

    string interpolation ig

--- terraform/iam.tf
+++ terraform/iam.tf
@@ -3,7 +3,7 @@ data "aws_iam_policy_document" "ping_pong_policy_document" {
         sid       = "PublicReadGetObject"
         effect    = "Allow"
         actions   = ["s3:GetObject"]
-        resources = [aws_s3_bucket.ping_pong.arn + "/*"]
+        resources = ["${aws_s3_bucket.ping_pong.arn}/*"]
 
         principals {
             identifiers = ["*"]
