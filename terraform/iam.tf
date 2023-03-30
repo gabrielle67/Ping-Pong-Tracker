b73b17b6bc7e375240202da2c8ddbded54690f85
gabrielle67 <gdyer8899@gmail.com>
2023-03-30 15:25:34 -0500

    iam policy syntax fix

--- terraform/iam.tf
+++ terraform/iam.tf
@@ -2,7 +2,7 @@ data "aws_iam_policy_document" "ping_pong_policy_document" {
     statement {
         sid = "PublicReadGetObject"
         effect = "Allow"
-        principal = "*"
+        principals = "*"
         actions = [
             "s3:GetObject"
         ]
