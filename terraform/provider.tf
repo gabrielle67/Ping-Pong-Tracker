0650f5860dd9f16edf9019a4255dc426f4642d85
gabrielle67 <gdyer8899@gmail.com>
2023-03-30 16:18:32 -0500

    add required providers

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,4 +1,21 @@
 
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "4.60.0"
+    }
+
+    cloud {
+      organization = "goob"
+
+      workspaces {
+        name = "Ping-Pong-Workspace"
+      }
+    }
+  } 
+}
+
 provider "aws" {
   region = "eu-east-1"
 }
