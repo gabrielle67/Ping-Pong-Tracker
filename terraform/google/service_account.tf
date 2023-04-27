0f20584c7c8aa61041e893f66e385732149a2263
gabrielle67 <gdyer8899@gmail.com>
2023-04-27 13:20:43 -0500

    update key algo

--- terraform/google/service_account.tf
+++ terraform/google/service_account.tf
@@ -5,5 +5,5 @@ resource "google_service_account" "ping_pong_service_account" {
 
 resource "google_service_account_key" "ping_pong_service_account_key" {
   service_account_id = google_service_account.ping_pong_service_account.id
-  key_algorithm      = "RSA"
+  key_algorithm      = "KEY_ALG_RSA_2048"
 }
