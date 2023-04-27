b831298d48aec5a8f01e3608ae0071a342f2c2b3
gabrielle67 <gdyer8899@gmail.com>
2023-04-27 13:37:44 -0500

    download acct key

--- terraform/google/service_account.tf
+++ terraform/google/service_account.tf
@@ -7,3 +7,12 @@ resource "google_service_account_key" "ping_pong_service_account_key" {
   service_account_id = google_service_account.ping_pong_service_account.id
   key_algorithm      = "KEY_ALG_RSA_2048"
 }
+
+data "google_service_account_key" "ping_pong_service_account_key" {
+  service_account_id = google_service_account.ping_pong_service_account.id
+  public_key_type    = "TYPE_X509_PEM_FILE"
+}
+
+output "service_account_key" {
+  value = data.google_service_account_key.ping_pong_service_account_key.private_key_data
+}
