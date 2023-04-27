8757cec405aa4b936d9823e06b6844e9c79f2d32
gabrielle67 <gdyer8899@gmail.com>
2023-04-27 13:56:07 -0500

    hmmm

--- terraform/google/service_account.tf
+++ terraform/google/service_account.tf
@@ -1,18 +1,20 @@
 resource "google_service_account" "ping_pong_service_account" {
-  account_id   = "ping-pong-svc-acct"
-  display_name = "Ping Pong Service Account"
+    account_id   = "ping-pong-svc-acct"
+    display_name = "Ping Pong Service Account"
 }
 
 resource "google_service_account_key" "ping_pong_service_account_key" {
-  service_account_id = google_service_account.ping_pong_service_account.id
-  key_algorithm      = "KEY_ALG_RSA_2048"
+    service_account_id = google_service_account.ping_pong_service_account.id
+    key_algorithm      = "KEY_ALG_RSA_2048"
+    private_key_type    = "TYPE_PKCS12_FILE"
+    private_key_length  = 2048
 }
 
 data "google_service_account_key" "ping_pong_service_account_key" {
-  service_account_id = google_service_account.ping_pong_service_account.id
-  public_key_type    = "TYPE_X509_PEM_FILE"
+    service_account_id = google_service_account.ping_pong_service_account.id
+    public_key_type    = "TYPE_X509_PEM_FILE"
 }
 
 output "service_account_key" {
-  value = data.google_service_account_key.ping_pong_service_account_key.private_key_data
+    value = google_service_account_key.ping_pong_service_account_key.private_key
 }
