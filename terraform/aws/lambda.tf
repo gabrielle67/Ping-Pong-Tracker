474c14366d95f55a42cb9e729abf88f0cf2d78eb
gabrielle67 <gdyer8899@gmail.com>
2023-07-01 20:38:55 -0500

    fix iam by adding arn

--- terraform/aws/lambda.tf
+++ terraform/aws/lambda.tf
@@ -6,7 +6,7 @@ data "archive_file" "lambda" {
 
 resource "aws_lambda_function" "get_highest_scorer" {
   function_name = "getHighestScorer"
-  role          = aws_iam_role.lambda_iam
+  role          = aws_iam_role.lambda_iam.arn
   handler       = "getHighestScorer.lambda_handler"
   runtime       = "python3.9"
   filename      = "lambda_output/lambda_functions/getHighestScorer/getHighestScorer.py"
