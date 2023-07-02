6193983461cfaac498371ad7851a1e9dec096a53
gabrielle67 <gdyer8899@gmail.com>
2023-07-02 16:30:40 -0500

    test lambda

--- terraform/lambda.tf
+++ terraform/lambda.tf
@@ -7,7 +7,7 @@ data "archive_file" "lambda" {
 resource "aws_lambda_function" "get_highest_scorer" {
   function_name = "getHighestScorer"
   role          = aws_iam_role.lambda_iam.arn
-  handler       = "getHighestScorer.lambda_handler"
+  handler       = "getHighestScorer/getHighestScorer.lambda_handler"
   runtime       = "python3.9"
-  filename      = "lambda_output/lambda_functions/getHighestScorer/getHighestScorer.py"
+  filename      = "getHighestScorer/getHighestScorer.py"
 }
