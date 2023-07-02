5d1d0ebb2c4fe938fdc902955cb7a32813ef1746
gabrielle67 <gdyer8899@gmail.com>
2023-07-01 21:24:49 -0500

    remove chdir

--- terraform/lambda.tf
+++ terraform/lambda.tf
@@ -1,6 +1,6 @@
 data "archive_file" "lambda" {
   type        = "zip"
-  source_dir  = "../../backend/lambda_functions"
+  source_dir  = "../backend/lambda_functions"
   output_path = "${path.module}/lambda_output.zip"
 }
 
