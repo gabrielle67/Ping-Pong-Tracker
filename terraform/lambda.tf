07242455f27b5141dd09122fae1d40d80a005472
gabrielle67 <gdyer8899@gmail.com>
2023-07-02 16:39:25 -0500

    fix lambda

--- terraform/lambda.tf
+++ terraform/lambda.tf
@@ -1,7 +1,7 @@
 data "archive_file" "lambda" {
   type        = "zip"
   source_dir  = "../backend/lambda_functions"
-  output_path = "${path.module}/lambda_output.zip"
+  output_path = "/tmp/lambda_output.zip"
 }
 
 resource "aws_lambda_function" "get_highest_scorer" {
@@ -9,5 +9,5 @@ resource "aws_lambda_function" "get_highest_scorer" {
   role          = aws_iam_role.lambda_iam.arn
   handler       = "getHighestScorer/getHighestScorer.lambda_handler"
   runtime       = "python3.9"
-  filename      = "getHighestScorer/getHighestScorer.py"
+  filename      = data.archive_file.lambda.output_path
 }
