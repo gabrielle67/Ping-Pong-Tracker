c49e524fa2b7eb885b13872f62e8f414cc2c52e1
gabrielle67 <gdyer8899@gmail.com>
2023-07-03 16:01:48 -0500

    testing change

--- backend/lambda_functions/getHighestScorer/getHighestScorer.py
+++ backend/lambda_functions/getHighestScorer/getHighestScorer.py
@@ -2,11 +2,10 @@ import sys
 
 sys.path.append('./utils')
 sys.path.append('./models')
-from constants import SHEET # noqa E402
-from players import Players # noqa E402
+#from constants import SHEET # noqa E402
+from player import Player # noqa E402
 
 
 def lambda_handler(event, context):
-    players = Players(SHEET)
-    highestScorer = players.getHighestScorer()
-    return highestScorer
+    player = Player('Sally')
+    return player.getName()
