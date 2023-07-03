704bd377dd290c2659196a6841dc406fff4343b7
gabrielle67 <gdyer8899@gmail.com>
2023-07-03 17:29:04 -0500

    add env vars

--- terraform/lambda.tf
+++ terraform/lambda.tf
@@ -4,12 +4,27 @@ data "archive_file" "lambda" {
   output_path = "lambda_output.zip"
 }
 
+variable "credentials" {
+    type        = string
+}
+
+variable "sheet_key" {
+    type        = string
+}
+
 resource "aws_lambda_function" "get_highest_scorer" {
   function_name = "getHighestScorer"
   role          = aws_iam_role.lambda_iam.arn
   handler       = "getHighestScorer/getHighestScorer.lambda_handler"
   runtime       = "python3.9"
   filename      = data.archive_file.lambda.output_path
+
+  environment {
+    variables = {
+      "CREDENTIALS" = var.credentials
+      "SHEET_KEY" = var.sheet_key
+    }
+  }
 }
 
 resource "aws_lambda_function_url" "get_highest_scorer_url" {
