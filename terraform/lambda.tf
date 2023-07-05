c14c2ed4e646b5561270da22f4f5e4fc3cc35da0
gabrielle67 <gdyer8899@gmail.com>
2023-07-05 01:16:32 -0500

    add new lambda

--- terraform/lambda.tf
+++ terraform/lambda.tf
@@ -37,3 +37,35 @@ resource "aws_lambda_function_url" "get_highest_scorer_url" {
     allow_methods     = ["*"]
   }
 }
+
+data "archive_file" "lambda" {
+  type        = "zip"
+  source_dir  = "../backend/package2"
+  output_path = "lambda_output.zip"
+}
+
+resource "aws_lambda_function" "get_wins_and_losses" {
+  function_name = "getAllWinsandLosses"
+  role          = aws_iam_role.lambda_iam.arn
+  handler       = "getAllWinsandLosses/getAllWinsandLosses.lambda_handler"
+  runtime       = "python3.9"
+  filename      = data.archive_file.lambda.output_path
+
+  environment {
+    variables = {
+      "CREDENTIALS" = var.credentials
+      "SHEET_KEY" = var.sheet_key
+    }
+  }
+}
+
+resource "aws_lambda_function_url" "get_wins_and_losses_url" {
+  function_name = aws_lambda_function.get_wins_and_losses.function_name
+  authorization_type = "NONE"
+
+  cors {
+    allow_credentials = false
+    allow_origins     = ["*"]
+    allow_methods     = ["*"]
+  }
+}
