81ff1e0423b6f0a007ef7a314639e84962d7ccee
gabrielle67 <gdyer8899@gmail.com>
2023-07-06 20:02:18 -0500

    getAllPlayerNames

--- backend/lambda_functions/getOpponentScores/getOpponentScores.py
+++ backend/lambda_functions/getOpponentScores/getOpponentScores.py
@@ -8,7 +8,7 @@ from models.players import Players # noqa E402
 
 def lambda_handler(event, context):
     players = Players(SHEET)
-    player = event.player
-    opp = event.opp
-    score = players.getPlayerByName(player).getScoreOpponent(opp)
+    player = event["name"]
+    opponent = event["opp"]
+    score = players.getPlayerByName(player).getScoreOpponent(opponent)
     return score
---
+++ terraform/getAllPlayerNames.tf
@@ -0,0 +1,31 @@
+data "archive_file" "all_player_names_zip" {
+  type        = "zip"
+  source_dir  = "../package_dirs/getAllPlayerNames_pkg"
+  output_path = "getAllPlayerNames_output.zip"
+}
+
+resource "aws_lambda_function" "get_all_player_names" {
+  function_name = "getAllWinsandLosses"
+  role          = aws_iam_role.lambda_iam.arn
+  handler       = "getAllPlayerNames/getAllPlayerNames.lambda_handler"
+  runtime       = "python3.9"
+  filename      = data.archive_file.all_player_names_zip.output_path
+
+  environment {
+    variables = {
+      "CREDENTIALS" = var.credentials
+      "SHEET_KEY" = var.sheet_key
+    }
+  }
+}
+
+resource "aws_lambda_function_url" "get_all_player_names_url" {
+  function_name = aws_lambda_function.get_all_player_names.function_name
+  authorization_type = "NONE"
+
+  cors {
+    allow_credentials = false
+    allow_origins     = ["*"]
+    allow_methods     = ["*"]
+  }
+}
